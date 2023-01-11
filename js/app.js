6b59c6a14557b84365ec8b1f51b67d4206b7524d
Sergio <smontufar250@gmail.com>
2023-01-10 21:48:43 -0500

    added a gradient background, added css to align text and buttons, and added a temporary border

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+
+window.onload = (event) => {
+    document.querySelector("form").addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const search = document.querySelector('input[type="text"]').value;
+        fetch("https://open.er-api.com/v6/latest/" + search).then((response) => {
+            console.log(response);
+            console.log(response.json());
+            // return response.json();
+
+        })
+    })
+}
+

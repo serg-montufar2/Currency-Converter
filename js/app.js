660e3e8a1111c5e762f4130dc5cbd65680ad438a
Sergio <smontufar250@gmail.com>
2023-01-12 20:29:11 -0500

    added the dropdown list to both options instead of just 1, but styling needs to be fixed

--- js/app.js
+++ js/app.js
@@ -1,22 +1,36 @@
-const data = fetch("https://open.er-api.com/v6/latest").then((response) => {
-//  console.log(response);
- return response.json();
-}).then((json) => {
-    // console.log(json.rates);
-    const keys = Object.keys(json.rates);
-    const select = document.getElementById("currencies");
-for (let i = 0; i < keys.length; i++) {
-    console.log(keys[i]);
-    const currencies = document.createElement("option");
-    currencies.setAttribute("class", "currency-code");
-    currencies.setAttribute("value", keys[i]);
-    currencies.innerText = keys[i];
-    select.append(currencies);
-}
-})
-// }).then((err) => {
-//     console.log(err, "this was an error");
-// }) 
+window.onload = (event) => {
+
+    fetch("https://open.er-api.com/v6/latest").then((response) => {
+        //  console.log(response);
+         return response.json();
+        }).then((json) => {
+            // console.log(json.rates);
+            console.log(json, "this was successful");
+            const keys = Object.keys(json.rates);
+            const select = document.querySelectorAll("select");
+            for (let i = 0; i < select.length; i++) {
+                for (let j = 0; j < keys.length; j++) {
+                    const currencies = document.createElement("option");
+                    currencies.setAttribute("class", "currency-code");
+                    currencies.setAttribute("value", keys[j]);
+                    currencies.innerText = keys[j];
+                    select[i].append(currencies)
+                }
+            }
+        },
+        (err) => {
+            console.log(err, "this was an error");
+        })
+    }
+    
+
+
+
+
+
+
+
+
 
 
 

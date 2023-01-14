3767015f67c6e045720e57cb2e9937897bcaee89
Sergio <smontufar250@gmail.com>
2023-01-14 17:26:31 -0500

    Was able to have the dropdown menus access the exchange rates within the code, change the font, remove some buttons, and made the second text box read-only

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
-window.onload = (event) => {
+// variables that are used throughout the code
+const apiURL = "https://open.er-api.com/v6/latest";
+const select = document.querySelectorAll("select");
 
-    fetch("https://open.er-api.com/v6/latest").then((response) => {
-        //  console.log(response);
+// This will have the code execute when the window loads
+window.onload = (event) => {
+    fetch(apiURL).then((response) => {
          return response.json();
         }).then((json) => {
-            // console.log(json.rates);
-            console.log(json, "this was successful");
+            // console.log(json, "this was successful");
+//This turns the keys of the exchange rates into an array
             const keys = Object.keys(json.rates);
-            const select = document.querySelectorAll("select");
+//This adds each of the keys or currency codes into the dropdown menus one by one
             for (let i = 0; i < select.length; i++) {
                 for (let j = 0; j < keys.length; j++) {
                     const currencies = document.createElement("option");
@@ -21,35 +24,30 @@ window.onload = (event) => {
         (err) => {
             console.log(err, "this was an error");
         })
-    }
-    
+}
 
 
+const submit = document.querySelector('input[type="submit"');
+    submit.addEventListener("click", (e) => {
+        e.preventDefault();
+        getRates();
+    });
 
 
+const inputValue = document.querySelector('input[type="number"]').value;
 
-
-
-
-
-
-
-// window.onload = (event) => {
-//     document.querySelector("select").addEventListener("submit", (e) => {
-//         e.preventDefault();
-
-//         const search = document.querySelector('input[type="text"]').value;
-//         fetch("https://open.er-api.com/v6/latest/" + search).then((response) => {
-//         return response.json();
-
-//         }).then((json) => {
-//         console.log(json, "this was successful");
-        
-
-//         },
-//         (error) => {
-//             console.log(error, "this was an error")
-//         })
-//     })
-// }
+const getRates = () => {
+    const firstValue = select[0].value;
+    const secondValue = select[1].value
+        fetch(apiURL + "/" + firstValue).then((response) => {
+            return response.json();
+        }).then((json) => {
+            console.log(json, "this was successful");
+            let rate = json.rates[secondValue];
+            console.log(rate);
+        },
+        (err) => {
+            console.log(err, "this was an error");
+        })
+}
 
